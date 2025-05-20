f9adc549cdb06c3498b907a04dd4c438d46d7066
Amr-7246 <aehab7246@gmail.com>
2025-05-21 00:45:10 +0300

    Done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import { Contact, StarsCanvas } from '../components'
 
 const ContactPage = () => {
 return (
-    <div className="relative pt-[100px] z-0">
+    <div className="relative min-h-screen pt-[100px] z-0">
         <Contact />
         <StarsCanvas />
     </div>
